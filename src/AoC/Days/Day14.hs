35de3902128d674ec2c0f9cefb49f8d42463ff9c
Zsolt Szende <zsolt@artificial.io>
2021-12-14 20:55:33 +0100

    tidy 14

--- src/AoC/Days/Day14.hs
+++ src/AoC/Days/Day14.hs
@@ -2,7 +2,6 @@ module AoC.Days.Day14 where
 
 import AoC.Lib.Parser
 import AoC.Prelude
-import Control.Monad.State.Strict
 import Data.Map.Strict qualified as Map
 
 parse :: String -> Maybe ([(Char, Char)], Rules)
@@ -19,27 +18,33 @@ type PairCounts = Map (Char, Char) Int
 type Rules = Map (Char, Char) Char
 
 solve :: Int -> ([(Char, Char)], Rules) -> Int
-solve n (l, rules) = checkSum (snd (last l)) $ times n (step rules) (toPairCounts l)
+solve n (l, rules) =
+  checkSum (snd (last l)) $
+    times n (step rules) (toPairCounts l)
 
 toPairCounts :: [(Char, Char)] -> PairCounts
-toPairCounts = foldr (\k m -> Map.insertWith (+) k 1 m) mempty
+toPairCounts = foldr (`incAtBy` 1) mempty
 
 step :: Rules -> PairCounts -> PairCounts
-step rules pairCounts =
-  let f :: (Char, Char) -> Int -> State PairCounts ()
-      f (a, b) n = case rules !? (a, b) of
-        Nothing -> pure ()
-        Just v -> modify $ Map.insertWith (+) (a, v) n . Map.insertWith (+) (v, b) n
-   in execState (Map.traverseWithKey f pairCounts) mempty
+step rules = Map.foldrWithKey f mempty
+  where
+    f :: (Char, Char) -> Int -> PairCounts -> PairCounts
+    f (a, b) n m = case rules !? (a, b) of
+      Nothing -> m
+      Just v -> incAtBy (a, v) n $ incAtBy (v, b) n m
 
 checkSum :: Char -> PairCounts -> Int
 checkSum lastChar pairCounts =
-  let ns = Map.elems (charCount pairCounts) in maximum ns - minimum ns
+  let ns = Map.elems (charCount pairCounts)
+   in maximum ns - minimum ns
   where
     charCount :: Map (Char, Char) Int -> Map Char Int
     charCount =
       Map.adjust (+ 1) lastChar
-        . Map.foldrWithKey (\(a, _) -> Map.insertWith (+) a) mempty
+        . Map.foldrWithKey (\(a, _) -> incAtBy a) mempty
+
+incAtBy :: (Ord a) => a -> Int -> Map a Int -> Map a Int
+incAtBy = Map.insertWith (+)
 
 inputP :: Parser ([(Char, Char)], Rules)
 inputP = do
